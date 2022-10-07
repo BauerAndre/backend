55d426ce703514adaade021346d5c94570f1684b
Andre Bauer <andrebauer@live.com>
2022-10-07 20:13:08 +0900

    conection error disconnection status'

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const server = express().listen(PORT, () => {
 });
 
 const socketHandler = socketIO(server);
-
-socketHandler.on("connection", () => {
+socketHandler.on("connection", (socket) => {
+  socket.on("connect_error", () => {
+    console.log("Connection error!");
+  });
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+  });
   console.log("Client Connected!");
 
   setInterval(() => {
