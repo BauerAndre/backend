ad812aab556f8b8024e8a3437da3afd85e32ee49
Andre Bauer <andrebauer@live.com>
2022-10-07 18:44:54 +0900

    web socket connection

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const PORT = 3000;
 const express = require("express");
+const socketIO = require("socket.io");
 
 const server = express().listen(PORT, () => {
   console.log(`Listening to ${PORT}`);
+});
+
+const socketHandler = socketIO(server);
+
+socketHandler.on("connection", () => {
+  console.log("Client Connected!");
 });
